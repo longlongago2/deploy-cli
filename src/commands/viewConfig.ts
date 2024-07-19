44a3fb947030f16bd067037b16cf69138a6f7e65
longlongago2 <1789136583@qq.com>
2024-07-19 15:57:47 +0800

    update: fix view config

--- src/commands/viewConfig.ts
+++ src/commands/viewConfig.ts
@@ -1,3 +1,4 @@
+import slash from 'slash';
 import { readDeployConfig } from '../utils.js';
 
 export interface ViewConfigOptions {
@@ -7,10 +8,22 @@ export interface ViewConfigOptions {
 export async function viewConfig(options: ViewConfigOptions): Promise<void> {
   const { config } = options;
   const result = await readDeployConfig(config);
-  // clear password in output
-  if (result.config.password) {
-    delete result.config.password;
+  if (!result.path) {
+    console.error('No config found');
+    return;
   }
+  const { host, port, tasks, privateKey } = result.config;
+  const output = {
+    path: slash(result.path),
+    config: {
+      host,
+      port,
+      username: '***(secret)',
+      password: '***(secret)',
+      privateKey,
+      tasks,
+    },
+  };
   // depth: null 是 console.dir 方法的一个选项，表示对象的递归深度。设置为 null 意味着没有深度限制，所有嵌套的属性都会被完全展开和显示。
-  console.dir(result, { depth: null, colors: true });
+  console.dir(output, { depth: null, colors: true });
 }
