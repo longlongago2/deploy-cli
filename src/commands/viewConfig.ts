e661f66adb269c901d9b8de57141481999bb799f
longlongago2 <1789136583@qq.com>
2024-07-19 15:30:05 +0800

    update: clear password in output

--- src/commands/viewConfig.ts
+++ src/commands/viewConfig.ts
@@ -8,5 +8,10 @@ export async function viewConfig(options: ViewConfigOptions): Promise<void> {
   const { config } = options;
   const result = await readDeployConfig(config);
   // depth: null 是 console.dir 方法的一个选项，表示对象的递归深度。设置为 null 意味着没有深度限制，所有嵌套的属性都会被完全展开和显示。
-  console.dir(result, { depth: null, colors: true });
+  const _result = { ...result };
+  if ('password' in _result && _result.password) {
+    // clear password in output
+    delete _result.password;
+  }
+  console.dir(_result, { depth: null, colors: true });
 }
